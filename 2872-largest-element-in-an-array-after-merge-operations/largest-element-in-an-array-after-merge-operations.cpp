6059870ddfff68c0dc634b1186e429d202d363a9
Muhammad Ahmad <mahmad.8962@gmail.com>
2024-02-10 00:04:19 +0500

    Time: 128 ms (54.01%) | Memory: 128.9 MB (5.35%) - LeetSync

---
+++ 2872-largest-element-in-an-array-after-merge-operations/largest-element-in-an-array-after-merge-operations.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long maxArrayValue(vector<int>& nums) {
+ long long ans=0;
+      
+        vector<long long> arr;
+        int size=nums.size();
+        for(int i=0;i<size;i++)
+            arr.push_back(nums[i]);   
+       for(int i=arr.size()-1;i>0;i--)
+          if(arr[i-1] <= arr[i])
+              arr[i-1] = arr[i] + arr[i-1];
+       sort(arr.begin(),arr.end());
+       return arr[arr.size()-1];
+    }
+};
